c78b6de7ed677aac21591facd9dba7601d87534a
Anastasiya <anastasyakobets@ukr.net>
2023-02-15 15:12:58 +0200

    hw_3

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -18,7 +18,7 @@ export const LoadBtn = styled.button`
   width: 180px;
   margin: 0 auto;
   box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+    0 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
   :hover,
   :focus {
     background-color: #303f9f;
